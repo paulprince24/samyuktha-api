e143b59a5ee3947052d75aa08600f90a4b43ea9d
Paul Prince <39607967+paulprince24542@users.noreply.github.com>
2023-10-18 17:07:38 +0530

    Build Updated

--- functions/api.js
+++ functions/api.js
@@ -1,15 +1,156 @@
 const express = require("express");
 const serverless = require("serverless-http");
-
+var cors = require("cors")
 const app = express();
 const router = express.Router();
 
+//Load Postgres Server
+var { client } = require("./db_config");
+const morgan = require("morgan");
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
 router.get("/", (req, res) => {
-  res.json({
-    hello: "hi",
-  });
+  res.send("API is running");
 });
 
-app.use("/.netlify/functions/api", router);
+// @Route http://localhost:5000/v1/api/events
+// @method GET
+
+router.get("/v1/api/events", async (req, res) => {
+  try {
+    var getEvents = await client.query(
+      "SELECT * from tbl_events INNER JOIN tbl_rules ON tbl_events.id = tbl_rules.eventid"
+    );
+    console.log(getEvents.rows);
+    res.status(200).json({
+      events: getEvents.rows,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      msg: "Internal Server Error",
+      err: err,
+    });
+  }
+});
+
+router.post("/v1/api/events/single", async (req, res) => {
+  console.log(req.body);
+  try {
+    var findParticipant = await client.query(
+      "SELECT * from tbl_single WHERE participantemail = $1 AND eventid = $2",
+      [req.body.participantemail, req.body.eventid]
+    );
+    console.log(findParticipant.rowCount);
+    if (findParticipant.rowCount == 0) {
+      var newParticipant = await client.query(
+        "INSERT INTO tbl_single(userid,eventid,participantemail,participantname, participantphone) VALUES($1,$2,$3,$4,$5)",
+        [
+          req.body.userid,
+          req.body.eventid,
+          req.body.participantemail,
+          req.body.participantname,
+          req.body.participantphone,
+        ]
+      );
+      console.log(newParticipant.rows);
+      res.status(200).json({
+        status: true,
+        msg: "Participant Added",
+      });
+    } else {
+      res.status(200).json({
+        status: false,
+        msg: "Pariticipant is already registered",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      msg: "Internal Server Error",
+      err: err,
+    });
+  }
+});
+
+router.post("/v1/api/events/group", async (req, res) => {
+  console.log(req.body);
+  try {
+    var findParticipant = await client.query(
+      "SELECT * FROM tbl_group WHERE participant1email = $1 AND eventid = $2",
+      [req.body.participant1email, req.body.eventid]
+    );
+    console.log(findParticipant.rowCount);
+    if (findParticipant.rowCount == 0) {
+      var newParticipant = await client.query(
+        "INSERT INTO tbl_group(userId,eventId,participant1Email,participant1Name,participant1Phone,participant2Name,participant2Phone,participant3Name,participant3Phone,participant4Name,participant4Phone,participant5Name,participant5Phone) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)",
+        [
+          req.body.userid,
+          req.body.eventid,
+          req.body.participant1email,
+          req.body.participant1name,
+          req.body.participant1phone,
+          req.body.participant2name || "",
+          req.body.participant2phone || "",
+          req.body.participant3name || "",
+          req.body.participant3phone || "",
+          req.body.participant4name || "",
+          req.body.participant4phone || "",
+          req.body.participant5name || "",
+          req.body.participant5phone || "",
+        ]
+      );
+      console.log(newParticipant.rows);
+      res.status(200).json({
+        status: true,
+        msg: "Team Added",
+      });
+    } else {
+      res.status(200).json({
+        status: false,
+        msg: "Team is already registered",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      msg: "Internal Server Error",
+      err: err,
+    });
+  }
+});
+
+router.get("/v1/api/events/group", async (req, res) => {
+  try {
+    var getRegisteredUser = await client.query("SELECT * FROM tbl_group");
+    res.status(200).json(getRegisteredUser.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(200).json({
+      msg: "Internal Server Error",
+    });
+  }
+});
+router.get("/v1/api/events/single", async (req, res) => {
+  try {
+    var getRegisteredUser = await client.query("SELECT * FROM tbl_single");
+    res.status(200).json(getRegisteredUser.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(200).json({
+      msg: "Internal Server Error",
+    });
+  }
+});
+
+app.use("/", router);
 
 module.exports.handler = serverless(app);
